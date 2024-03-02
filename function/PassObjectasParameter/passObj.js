62795086519209362eb5763d46bdc261b7a2057f
Vishal <vishal.bhumca21@gmail.com>
2024-03-02 12:18:11 +0530

    passed object function as argumrnt

---
+++ function/PassObjectasParameter/passObj.js
@@ -0,0 +1,16 @@
+// function objpass(acceptObj){
+//     console.log("age",acceptObj.age)
+// }
+// let user={name:"vishal",age:20};
+// objpass(user);
+
+// pass object with default Value
+function EmptyObj(obj={first:"User1"}){
+    console.log(obj.first)
+}
+let obj={
+    first:"vishal",
+    age:26
+}
+EmptyObj()
+
