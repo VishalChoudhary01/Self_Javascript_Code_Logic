a0a450e9a97521c84a9724d8183d9dfe3f350ed1
Vishal <vishal.bhumca21@gmail.com>
2025-01-31 18:55:53 +0530

    Refresh Javascript

---
+++ Frontend_Master/Javascript/001dot_double_dot_method.js
@@ -0,0 +1,13 @@
+const num=100;
+console.log(num);
+console.log(num.toString())
+// console.log(100.toString()); This line show error
+// Error:  An identifie or keyword cannot immediate follow a numeric literal
+
+// solution 1
+console.log((100).toString());//Output:100
+
+// solution 2 Double dot
+console.log(100..toString());//Output 100
+
+
